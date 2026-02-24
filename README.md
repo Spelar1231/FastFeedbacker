6dbc80587f6172ce7663c9400fc196323a40123e
Alma <171142732+Spelar1231@users.noreply.github.com>
2026-02-24 19:26:17 +0500

    Add project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+This mod makes the feedbacker faster by removing the cooldown. That's all.
+
+Made by: Alma
---
+++ RemoveCooldown.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Fast_Feedbacker
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class RemoveCooldown : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger { get; private set; } = null!;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin Fast Feedbacker is loaded!");
+            gameObject.hideFlags = HideFlags.DontSaveInEditor;
+            Harmony.CreateAndPatchAll(typeof(RemoveCooldown));
+        }
+
+        [HarmonyPostfix][HarmonyPatch(typeof(Punch), "Start")]
+        public static void Postfix(Punch __instance)
+        {
+            if (__instance.type == FistType.Standard)
+                AccessTools.Field(typeof(Punch), "cooldownCost").SetValue(__instance, 0f);
+        }
+    }
+}
